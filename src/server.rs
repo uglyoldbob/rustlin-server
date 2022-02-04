284418de39357645dba5b2f5de4547fddd9bcb26
Thomas Epperson <thomas.epperson@gmail.com>
2022-02-04 14:38:46 -0500

    Server will now send the very first packet to the client that is connected. Encryption not yet implemented.

--- src/server.rs
+++ src/server.rs
@@ -5,6 +5,73 @@ use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
 use std::fmt;
 
+use std::convert::TryInto;
+use std::vec::Vec;
+
+struct Packet {
+	data: Vec<u8>,
+}
+
+impl Packet {
+	fn new() -> Packet {
+		Packet {
+			data: Vec::new(),
+		}
+	}
+	fn len(&self) -> u16 {
+		self.data.len().try_into().unwrap()
+	}
+	fn buf(&self) -> Vec<u8> {
+		self.data.to_vec()
+	}
+	fn add_u8(&mut self, d: u8) -> &mut Packet {
+		self.data.push(d);
+		self
+	}
+	fn add_u16(&mut self, d: u16) -> &mut Packet {
+		self.data.append(&mut d.to_le_bytes().to_vec());
+		self
+	}
+	fn add_u32(&mut self, d: u32) -> &mut Packet {
+		self.data.append(&mut d.to_le_bytes().to_vec());
+		self
+	}
+}
+
+struct ServerPacketReceiver {
+	reader: tokio::net::tcp::OwnedReadHalf,
+	decryption_key: Option<u32>,
+}
+
+impl ServerPacketReceiver {
+	fn new(r: tokio::net::tcp::OwnedReadHalf) -> ServerPacketReceiver {
+		ServerPacketReceiver {
+			reader: r,
+			decryption_key: None,
+		}
+	}
+}
+
+struct ServerPacketSender {
+	writer: tokio::net::tcp::OwnedWriteHalf,
+	encryption_key: Option<u32>,
+}
+
+impl ServerPacketSender {
+	fn new(w: tokio::net::tcp::OwnedWriteHalf) -> ServerPacketSender {
+		ServerPacketSender {
+			writer: w,
+			encryption_key: None,
+		}
+	}
+	
+	async fn send_packet(&mut self, data: Packet) -> Result<(), ClientError> {
+		self.writer.write_u16_le(data.len()+2).await?;
+		self.writer.write(&data.buf()).await?;
+		Ok(())
+	}
+}
+
 #[derive(Debug,Clone)]
 struct ClientError;
 impl fmt::Display for ClientError {
@@ -20,6 +87,16 @@ impl From<std::io::Error> for ClientError {
 }
 
 async fn process_client(mut socket: tokio::net::TcpStream) -> Result<u8, ClientError> {
+	let (reader, writer) = socket.into_split();
+	let mut packet_reader = ServerPacketReceiver::new(reader);
+	let mut packet_writer = ServerPacketSender::new(writer);
+
+	let mut key_packet = Packet::new();
+	key_packet.add_u8(65)
+		.add_u32(0x12345678);
+	packet_writer.send_packet(key_packet).await?;
+	//TODO initialize encryption keys using the seed just transmitted to the client
+
     Ok(0)
 }
 
