1e2233bcc4add13cfae9456cb5c0dbe62d68c87f
Thomas Epperson <thomas.epperson@gmail.com>
2022-07-29 13:58:10 -0400

    Fix another stack overflow.

--- client/src/startup.rs
+++ client/src/startup.rs
@@ -111,11 +111,11 @@ pub fn startup(mode: DrawMode) {
         "Struct GameResources has size {}",
         std::mem::size_of::<GameResources>()
     );
-
+    dbg!(stacker::remaining_stack());
     let rt = tokio::runtime::Runtime::new().unwrap();
     let (mut s1, r1) = tokio::sync::mpsc::channel(100);
     let (s2, mut r2) = tokio::sync::mpsc::channel(100);
-    rt.spawn(async_main(r1, s2));
+    rt.spawn(std::pin::Pin::from(Box::new(async_main(r1, s2))));
 
     println!("Loading resources from {}", resources);
 
